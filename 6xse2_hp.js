5b10b34d0d05d057182b02e4c7ff1763fd6f44c7
eslyna <b020610088@student.utem.edu.my>
2018-12-31 14:26:34 +0800

    Update 6xse2_hp.js

--- 6xse2_hp.js
+++ 6xse2_hp.js
@@ -1,5 +1,39 @@
 var items = [
-    [601169694041, 33],
-    [601169699151, 33],
-    [601160941315, 34]
+   
+//    [6011 6969 9151, 33],      // enq cnk 2  Jenny
+//    [6011 6094 1315, 34],      // enq cnk 3  Dhia
+//    [6011 xxxx xxxx, 34],      // 
+       
+//    [6011 6417 3867, 33],     // enq cnk 5  Sharon
+//    [6011 6404 0533, 34],     // enq cnk 4  Elena
+//    [6018 281 7298, 34],      // enq cnk 6
+//    [6018 236 7352, 34],      // enq cnk 9
+//    [6011 xxxx xxxx, 34],     // 
+    
+//    [60 11-6414 3141, 33],     // enq cnk 7
+//    [60 11-6417 8717, 34],     // enq cnk 8
+//    [60 18 XXX XXXX, 34],      // enq cnk 9
+    
+    
+    
+     [6011 6969 4041, 33]  // enq cnk 1  Main
+    
+    
 ];
+
+/*
+
+Thease are the active phone numbers. 
+Please remove the inactive numbers from here.
+
+Shift 1
+Starts 11.30pm Ends 7.30pm
+
+Shift 2
+Starts 7.30am Ends 3.30pm
+ 
+Shift 3
+Starts 3.30am Ends 11.30pm
+
+*/           
+    
